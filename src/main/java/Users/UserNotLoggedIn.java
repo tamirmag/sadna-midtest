002cf78b9dee8c636c7db305d26bb1831892d7cc
rzarviv <rzarviv@gmail.com>
2017-04-27 22:56:39 +0300

    added a new exception : UserNotLoggedIn

--- src/main/java/Users/UserNotLoggedIn.java
+++ src/main/java/Users/UserNotLoggedIn.java
@@ -5,7 +5,7 @@ import Loggers.IErrorLogger;
 
 public class UserNotLoggedIn extends Exception {
     public UserNotLoggedIn(String username) {
-        super();
+        super("username "+username+" is not logged in");
         IErrorLogger.getInstance().writeToFile("username "+username+" is not logged in");
     }
 }
